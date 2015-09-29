662c4888dc5f91409fefcb711ea05e141617eb14
viktoriatibenska <vtibensk8@gmail.com>
2015-09-29 15:52:09 +0200

    rrrr

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <link rel="stylesheet" type="text/css" href="css/Joint.css" />
+	<script src="js/jquery.min.js"></script>
+	<script src="js/lodash.min.js"></script>
+	<script src="js/backbone-min.js"></script>
+	<script src="js/Joint.js"></script>
+	<script src="js/joint.shapes.uml.js"></script>
+	
+</head>
+<body>
+asd
+  <div id="myholder"></div>
+   <script src="js/StateChart.js"></script>
+</body>
+</html>
