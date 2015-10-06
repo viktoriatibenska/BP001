05339d95438db7f8d3dcb3c439b745c658753f59
viktoriatibenska <vtibensk8@gmail.com>
2015-10-06 16:04:17 +0200

    Added statetest.html: test of adding states to graph with a button.

--- index.php
+++ index.php
@@ -1,17 +1,18 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <link rel="stylesheet" type="text/css" href="css/Joint.css" />
-	<script src="js/jquery.min.js"></script>
-	<script src="js/lodash.min.js"></script>
-	<script src="js/backbone-min.js"></script>
-	<script src="js/Joint.js"></script>
-	<script src="js/joint.shapes.uml.js"></script>
-	
+<link rel="stylesheet" type="text/css" href="css/Joint.css" />
+<script src="js/jquery.min.js"></script>
+<script src="js/lodash.min.js"></script>
+<script src="js/backbone-min.js"></script>
+<script src="js/Joint.js"></script>
+<script src="js/joint.shapes.uml.js"></script>
+
 </head>
 <body>
-asd
-  <div id="myholder"></div>
-   <script src="js/StateChart.js"></script>
+	<a href="statetest.html">State adding test</a>
+	
+	<div id="myholder"></div>
+	<script src="js/StateChart.js"></script>
 </body>
 </html>
