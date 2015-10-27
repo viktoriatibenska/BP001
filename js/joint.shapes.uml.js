8139525157bdc2f7bb586d9cd923ad6df50fb28f
viktoriatibenska <vtibensk8@gmail.com>
2015-10-27 16:59:09 +0100

    Added linking arrow and visibility on hover to state

--- js/joint.shapes.uml.js
+++ js/joint.shapes.uml.js
@@ -326,6 +326,97 @@ joint.shapes.uml.Transition = joint.dia.Link.extend({
     }
 });
 
+joint.shapes.uml.CustomState = joint.shapes.basic.Generic.extend({
+
+    markup: [
+        '<g class="rotatable">',
+          '<g class="scalable">',
+          	'<g class="wrapped">',
+          		'<rect class="uml-state-wrapper"/>',
+          		'<image class="state-connector"/>',
+          	'</g>',
+            '<rect class="uml-state-body"/>',            
+          '</g>',
+          '<path class="uml-state-separator"/>',
+          '<text class="uml-state-name"/>',
+          '<text class="uml-state-events"/>',
+        '</g>'
+    ].join(''),
+
+    defaults: joint.util.deepSupplement({
+
+        type: 'uml.CustomState',
+
+        attrs: {
+        	'.wrapped': {
+        		'visibility': 'hidden'
+        	},
+        	'.state-connector': {
+        		'ref-x': 170, 'ref-y': 78, width: 25, height: 25 //, ref: 'rect'
+        	},
+        	'.uml-state-wrapper': {
+                'width': 200, 'height': 260, 'rx': 10, 'ry': 10,
+                'fill': '#ecf0f1', 'stroke': '#bdc3c7', 'stroke-width': 3
+            },
+            '.uml-state-body': {
+                'width': 200, 'height': 200, 'rx': 10, 'ry': 10,
+                'fill': '#ecf0f1', 'stroke': '#bdc3c7', 'stroke-width': 3
+            },
+            '.uml-state-separator': {
+                'stroke': '#bdc3c7', 'stroke-width': 2
+            },
+            '.uml-state-name': {
+                'ref': '.uml-state-body', 'ref-x': .5, 'ref-y': 5, 'text-anchor': 'middle',
+                'fill': '#000000', 'font-family': 'Courier New', 'font-size': 14
+            },
+            '.uml-state-events': {
+                'ref': '.uml-state-separator', 'ref-x': 5, 'ref-y': 5,
+                'fill': '#000000', 'font-family': 'Courier New', 'font-size': 14
+            }
+        },
+
+        name: 'State',
+        events: []
+
+    }, joint.shapes.basic.Generic.prototype.defaults),
+
+    initialize: function() {
+
+        this.on({
+            'change:name': this.updateName,
+            'change:events': this.updateEvents,
+            'change:size': this.updatePath
+        }, this);
+
+        this.updateName();
+        this.updateEvents();
+        this.updatePath();
+
+        joint.shapes.basic.Generic.prototype.initialize.apply(this, arguments);
+    },
+
+    updateName: function() {
+
+        this.attr('.uml-state-name/text', this.get('name'));
+    },
+
+    updateEvents: function() {
+
+        this.attr('.uml-state-events/text', this.get('events').join('\n'));
+    },
+
+    updatePath: function() {
+
+        var d = 'M 0 20 L ' + this.get('size').width + ' 20';
+
+        // We are using `silent: true` here because updatePath() is meant to be called
+        // on resize and there's no need to to update the element twice (`change:size`
+        // triggers also an update).
+        this.attr('.uml-state-separator/d', d, { silent: true });
+    }
+
+});
+
 if (typeof exports === 'object') {
 
     module.exports = joint.shapes.uml;
