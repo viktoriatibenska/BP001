2c8977d3e97bd525a287cb6a2bce57aa7d1d8275
viktoriatibenska <vtibensk8@gmail.com>
2016-02-15 18:52:58 +0100

    MVC first attempt

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <!DOCTYPE html>
 <html>
 <head>
+<meta charset="UTF-8">
 <link rel="stylesheet" type="text/css" href="css/Joint.css" />
 <script src="js/jquery.min.js"></script>
 <script src="js/lodash.min.js"></script>
@@ -12,7 +13,18 @@
 <body>
 	<a href="statetest.html">State adding test</a>
 	
-	<div id="myholder"></div>
-	<script src="js/StateChart.js"></script>
+	<h1>MVC test</h1>
+	
+	<?php
+	$model = new Model();
+	$controller = new Controller($model);
+	$view = new View($controller, $model);
+	
+	
+	echo $view->output();
+	?>
+	
+	<!-- <div id="myholder"></div>
+	<script src="js/StateChart.js"></script> -->
 </body>
 </html>
---
+++ model/Model.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+
+<?php
+class Model
+{
+    public $string;
+
+    public function __construct(){
+        $this->string = 'MVC + PHP = Awesome, click here!';
+    }
+
+}
